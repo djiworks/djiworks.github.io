8e8c218ef84beebeb1725e88e2f877c50398437c
Djothi Grondin <djothi@MacBook-Pro-de-Djothi.local>
2020-01-12 15:38:47 +0400

    Updates

---
+++ precache-manifest.b9c0aae49e2819cd24dfc3427bb4a567.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e3a8513754bb88ef8d622dcab3203e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2433b7f604782762796",
+    "url": "/static/css/main.47849b3e.chunk.css"
+  },
+  {
+    "revision": "da6e2a1c17d14c2e6f18",
+    "url": "/static/js/2.90e467c7.chunk.js"
+  },
+  {
+    "revision": "d2433b7f604782762796",
+    "url": "/static/js/main.48dff3e0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aebd5c418e3a8ae84cb20e0d28304545",
+    "url": "/static/media/bg9.aebd5c41.jpg"
+  },
+  {
+    "revision": "53e8c4aaa8365b37a32d2337ed9eb1de",
+    "url": "/static/media/forest.53e8c4aa.jpg"
+  },
+  {
+    "revision": "df7d52f9c6394c9853f280661cdf495d",
+    "url": "/static/media/handshake.df7d52f9.jpg"
+  },
+  {
+    "revision": "43707ee465f377e7df5aae2be06c5843",
+    "url": "/static/media/marseille.43707ee4.jpg"
+  },
+  {
+    "revision": "8b56f152a89e159521b8bac302371799",
+    "url": "/static/media/maurice.8b56f152.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "5586c8635f71f10ad3f64b07a17c33c6",
+    "url": "/static/media/ryan.5586c863.jpg"
+  },
+  {
+    "revision": "e7c4471fcdbd13d9cac11207658e6479",
+    "url": "/static/media/savane.e7c4471f.jpg"
+  }
+]);
